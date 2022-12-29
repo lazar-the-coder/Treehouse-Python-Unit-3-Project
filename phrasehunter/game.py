bc5d5869ff8962021677a69ee8cc2aa169b9f770
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2022-12-29 15:26:16 -0600

    final draft
    good

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -4,7 +4,7 @@ import string
 from phrasehunter import phrase
 
 
-phrase_list_1 = [
+phrase_list = [
     'gad lad',
     'cat in the hat',
     'bobby fink',
@@ -12,13 +12,10 @@ phrase_list_1 = [
     'John is a master'
     ]
 
-phrase_list = [
-    'gad lad'
-    ]
-
 class Game:
     def __init__(self):
         self.missed = 0
+        self.limit = 5
         self.phrases = phrase_list
         self.active_phrase = None
         self.guesses = []
@@ -27,13 +24,16 @@ class Game:
         self.welcome()
         self.active_phrase = phrase.Phrase(self.get_random_phrase())
         playing_game = True
-        self.active_phrase.display(self.guesses)
         while playing_game:
-            self.get_guess()
             self.active_phrase.display(self.guesses)
+            self.get_guess()
             if self.active_phrase.check_complete(self.guesses):
                 playing_game = False
-        self.game_over()
+                win = True
+            if self.missed == self.limit:
+                playing_game = False
+                win = False
+        self.game_over(win)
 
     def welcome(self):
         print('Welcome to the game')
@@ -42,19 +42,35 @@ class Game:
         return random.choice(phrase_list)
 
     def get_guess(self):
-        guess = input('make a guess' )
-        if guess in self.guesses:
-            print("You already guessed that")
-        elif guess in string.ascii_uppercase or guess in string.ascii_lowercase:
-            if self.active_phrase.check_letter(guess):
+        guess = input('make a guess' ).lower()
+        if len(guess) != 1:
+            print("please enter 1 letter as a guess, no more, no less.")
+        elif guess.lower() in string.ascii_lowercase:
+            if guess in self.guesses:
+                print("You already guessed that")
+            elif self.active_phrase.check_letter(guess):
                 print("Correct!")
                 self.guesses.append(guess)
             else:
-                print("Try Again!")
+                print("Wrong!")
+                self.missed += 1
+                print(f"You've answered wrong {self.missed} out of {self.limit} times")
         else:
             print('not possible')
 
-    def game_over(self):
-        print('Congradulations, you won!')
+    def game_over(self, win):
+        if win:
+            print('Congradulations, you won!')
+        else:
+            print('You have failed!')
+        while True:
+            play_again = input("would you like to play again? y/n").lower()
+            if play_again == "y":
+                break
+            else:
+                print('Alright, good bye!')
+                quit()
+        self.start()
+
 
 
